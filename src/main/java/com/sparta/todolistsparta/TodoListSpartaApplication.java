1fc30b23768413bd4619f64e481c32ef00c3a8c1
chamcham0707 <cheamin6306@naver.com>
2024-05-16 20:25:27 +0900

    init commit

---
+++ src/main/java/com/sparta/todolistsparta/TodoListSpartaApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.todolistsparta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoListSpartaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TodoListSpartaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sparta/todolistsparta/TodoListSpartaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.todolistsparta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoListSpartaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
